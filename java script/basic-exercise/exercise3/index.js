1fa3793313b57e828e5e33de25d31282a4a58bf4
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-07 23:43:21 -0700

    exercise 4

--- java script/basic-exercise/exercise3/index.js
+++ java script/basic-exercise/exercise3/index.js
@@ -1,7 +1,62 @@
-let isSunny = true;
-let isWeekend = false;
+// let isSunny = true;
+// let isWeekend = false;
 
-console.log(isSunny && isWeekend)
-console.log(isSunny || isWeekend)
-console.log(!isSunny)
-console.log(!isWeekend)
+// console.log(isSunny && isWeekend)
+// console.log(isSunny || isWeekend)
+// console.log(!isSunny)
+// console.log(!isWeekend)
+
+// let firstName = "ahmed";
+// let lastName = "nuur";
+// let greering = "hello";
+// console.log(firstName + "  " + lastName)
+// let greeting = "hello,"
+// greeting += " " + firstName,lastName
+// greeting += " " +lastName
+// console.log(greeting)
+//  add();
+// function add(){
+//     a = 10;
+//     b = 20;
+//     console.log(a + b)
+   
+// }
+// function great(name){
+//     console.log('hello mr/mrs '+ name)
+// }
+// great("ahmed")
+// function add(b){
+//     // console.log(a)
+//     console.log(b)
+
+// }
+// add(5+5)
+
+// function calculateTotal(price,quantity){
+//   return price * quantity
+
+
+// }
+// let letters = calculateTotal(1.03,40)
+// console.log("price: $1.03")
+// console.log('money :  $'+ letters)
+// console.log(" letters: "+ letters/ 1.03)
+
+// // avaiod function
+// function add (){  
+//   a=10
+//   b=20
+// }
+// // function expression
+// let functionName = function() {
+//   n = 10
+//   h =20;
+ 
+// }
+// let salaan =(a)=>{
+  
+//   console.log(a)
+
+// }
+// salaan("20")
+
---
+++ java script/basic-exercise/exercise4/index.js
@@ -0,0 +1,14 @@
+function add(a,b){
+    return a + b
+}
+console.log(add(10,20))
+console.log(add(30,40))
+console.log(add(100,10))
+
+// function expression
+let = function add(a,b){
+    return a + b
+}
+console.log(add(10,20))
+console.log(add(30,40))
+console.log(add(100,10))
