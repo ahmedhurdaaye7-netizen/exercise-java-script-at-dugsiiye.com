6ac51ddf99e121f29b3d3574d8839fca02350597
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-07 23:53:05 -0700

    ex5

---
+++ java script/basic-exercise/ex5/index.js
@@ -0,0 +1,8 @@
+let functionAdd = (a,b)=>{
+    return a + b
+}
+console.log(functionAdd(10,20))
+console.log(functionAdd(30,40))
+console.log(functionAdd(100,10))
+console.log(functionAdd(50,50))
+console.log(functionAdd(70,90))
