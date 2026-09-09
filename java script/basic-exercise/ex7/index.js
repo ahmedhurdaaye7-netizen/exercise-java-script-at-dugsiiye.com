484f83c708317ed034fab78bc01aaf024f1536b3
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-08 23:21:57 -0700

    ex7

---
+++ java script/basic-exercise/ex7/index.js
@@ -0,0 +1,9 @@
+let colors =["black","white","red","yellow"]
+//  before
+console.log(colors)
+colors.push("grey")
+console.log(colors)
+colors.shift()
+console.log(colors)
+console.log(colors.length)
+
