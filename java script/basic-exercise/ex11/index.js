382d99f056ae2cebda540844d79b4bb7cc086bf1
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-09 23:39:48 -0700

    ex12

---
+++ java script/basic-exercise/ex11/index.js
@@ -0,0 +1,59 @@
+
+
+
+// for(let number = 1;number < 51; number++){
+//     console.log(number)
+
+// }
+// let students = ["farax","nuur","geedi", "farax","nuur","geedi", "farax","nuur","geedi", "farax","nuur","geedi", "farax","nuur","geedi", "farax","nuur","geedi", "farax","nuur","geedi","kaani"]
+// for(let i = 0; i<students.length; i++){
+//     console.log(students)
+
+// }
+let students =[
+
+    {
+        name:"ahmed maxamed ahmed",
+        id: 36665,
+        facult: "computer scinec and it"
+        },
+         {
+        name:"ahmed maxamed ahmed",
+        id: 36665,
+        facult: "computer scinec and it"
+        },
+         {
+        name:"ahmed maxamed ahmed",
+        id: 36665,
+        facult: "computer scinec and it"
+        },
+         {
+        name:"ahmed maxamed ahmed",
+        id: 36665,
+        facult: "computer scinec and it"
+        },
+         {
+        name:"ahmed maxamed ahmed",
+        id: 36665,
+        facult: "computer scinec and it"
+        },
+         {
+        name:"ahmed maxamed ahmed",
+        id: 36665,
+        facult: "computer scinec and it"
+        },
+         {
+        name:"ahmed maxamed ahmed",
+        id: 36665,
+        facult: "computer scinec and it"
+        },
+         {
+        name:"ahmed maxamed ahmed",
+        id: 36665,
+        facult: "computer scinec and it"
+        },
+]
+for(let i = 0; i <students.length; i++){
+    console.log(students[i])
+
+}
---
+++ java script/basic-exercise/ex12/index.js
@@ -0,0 +1,6 @@
+let fruits =["apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange","apple","banana","lemon","orange",]
+
+for(let i = 0;i<fruits.length; i++){
+    console.log(fruits[i])
+
+}
