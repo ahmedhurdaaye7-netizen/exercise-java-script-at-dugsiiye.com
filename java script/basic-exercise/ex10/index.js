ab027bb16cc98e916ff2489ca71d995792d0ead8
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-09 22:20:11 -0700

    ex10

---
+++ java script/basic-exercise/ex10/index.js
@@ -0,0 +1,32 @@
+let libarary =[
+
+{
+title: "physics",
+author: "eng nuur",
+year: 2021
+
+},
+{
+title: "somali",
+author: "hassan cumar",
+year: 2022
+
+},
+{
+title: "java",
+author: "eng mc hamuuda",
+year: 2023
+
+},
+{
+title: "python",
+author: "hassan farax",
+year: 2020
+
+}
+
+
+]
+console.log(libarary)
+console.log(libarary[0].title)
+console.log(libarary[1].author)
