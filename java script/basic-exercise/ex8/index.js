668b63be2cbd1e060bc142783e245983b49ad896
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-09 22:10:14 -0700

    ex9

--- java script/basic-exercise/ex8/index.js
+++ java script/basic-exercise/ex8/index.js
@@ -5,6 +5,53 @@ year: 2004
 
 
 }
-console.log(car.make)
-console.log(car.model)
-console.log(car.year)
+// console.log(car.make)
+// console.log(car.model)
+// console.log(car.year)
+// car.model="harryy"
+// car.year = 2000,
+// car.make ="USA"
+// car.producedate ="2000-05-10|"
+
+// console.log(car)
+let students =[
+
+{
+    name: "ahmed maxamed ahmed",
+    id: 36665,
+    faculty: "computer siceince and it",
+    dep: "software engneering",
+    shift: "part time",
+    age: 22,
+    address: "garasbaley",
+
+},
+{
+    name: "nuur maxamed ahmed",
+    id: 36666,
+    faculty: "computer siceince and it",
+    dep: "software engneering",
+    shift: "part time",
+    age: 21,
+    address: "garasbaley",
+
+},
+{
+    name: "farax maxamed ahmed",
+    id: 36667,
+    faculty: "computer siceince and it",
+    dep: "software engneering",
+    shift: "part time",
+    age: 21,
+    address: "garasbaley",
+
+},
+
+
+
+]
+// console.log(students[1].id)
+// students[1].name="geedi"
+// console.log(students)
+// students[0].po
+
---
+++ java script/basic-exercise/ex9/index.js
@@ -0,0 +1,9 @@
+let car ={
+make: "jaban",
+model: "toyota",
+year: 2004
+
+
+}
+car.started ="1996-02-10"
+console.log(car)
