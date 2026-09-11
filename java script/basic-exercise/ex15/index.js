ec8e075fff552cf4b3de14a36951e56377f96991
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-11 10:22:04 -0700

    ex15

---
+++ java script/basic-exercise/ex15/index.js
@@ -0,0 +1,28 @@
+const people=[
+    {
+       name: "alice" ,
+       age:22,
+       city: "wonder land",
+
+
+    },
+      {
+       name: "nuur" ,
+       age:31,
+       city: "mogadishu",
+
+
+    },
+      {
+       name: "ahmed" ,
+       age:22,
+       city: "marko",
+
+
+    },
+]
+console.log("these are our customers")
+for(let person of people){
+    console.log(person)
+
+}
