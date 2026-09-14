b4b5963658dd5ed23faef283f27eab7212256650
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-14 06:50:01 -0700

    ex19

--- java script/basic-exercise/ex14/index.js
+++ java script/basic-exercise/ex14/index.js
@@ -1,6 +1,211 @@
 
-    let tiro ;
-    do{
-        tiro = Number(prompt("enter number greather than 10"));
-    } while(tiro<=10);
-        console.log("welcome")
+    // let tiro ;
+    // do{
+    //     tiro = Number(prompt("enter number greather than 10"));
+    // } while(tiro<=10);
+    //     console.log("welcome")
+    const students = [
+    {
+        id: 1,
+        name: "Ahmed",
+        age: 21,
+        gender: "Male",
+        course: "Software Engineering",
+        year: 2,
+        grade: 85,
+        city: "Mogadishu"
+    },
+    {
+        id: 2,
+        name: "Amina",
+        age: 20,
+        gender: "Female",
+        course: "Computer Science",
+        year: 2,
+        grade: 92,
+        city: "Hargeisa"
+    },
+    {
+        id: 3,
+        name: "Mohamed",
+        age: 23,
+        gender: "Male",
+        course: "Information Technology",
+        year: 3,
+        grade: 76,
+        city: "Kismayo"
+    },
+    {
+        id: 4,
+        name: "Fatima",
+        age: 19,
+        gender: "Female",
+        course: "Software Engineering",
+        year: 1,
+        grade: 88,
+        city: "Mogadishu"
+    },
+    {
+        id: 5,
+        name: "Abdullahi",
+        age: 22,
+        gender: "Male",
+        course: "Cybersecurity",
+        year: 3,
+        grade: 67,
+        city: "Garowe"
+    },
+    {
+        id: 6,
+        name: "Hodan",
+        age: 21,
+        gender: "Female",
+        course: "Computer Science",
+        year: 2,
+        grade: 95,
+        city: "Mogadishu"
+    },
+    {
+        id: 7,
+        name: "Yusuf",
+        age: 24,
+        gender: "Male",
+        course: "Software Engineering",
+        year: 4,
+        grade: 81,
+        city: "Bosaso"
+    },
+    {
+        id: 8,
+        name: "Maryan",
+        age: 20,
+        gender: "Female",
+        course: "Information Technology",
+        year: 2,
+        grade: 73,
+        city: "Mogadishu"
+    },
+    {
+        id: 9,
+        name: "Omar",
+        age: 22,
+        gender: "Male",
+        course: "Data Science",
+        year: 3,
+        grade: 90,
+        city: "Hargeisa"
+    },
+    {
+        id: 10,
+        name: "Khadija",
+        age: 21,
+        gender: "Female",
+        course: "Software Engineering",
+        year: 3,
+        grade: 87,
+        city: "Mogadishu"
+    },
+    {
+        id: 11,
+        name: "Ismail",
+        age: 19,
+        gender: "Male",
+        course: "Computer Science",
+        year: 1,
+        grade: 69,
+        city: "Beledweyne"
+    },
+    {
+        id: 12,
+        name: "Sahra",
+        age: 23,
+        gender: "Female",
+        course: "Cybersecurity",
+        year: 4,
+        grade: 94,
+        city: "Garowe"
+    },
+    {
+        id: 13,
+        name: "Hassan",
+        age: 22,
+        gender: "Male",
+        course: "Software Engineering",
+        year: 2,
+        grade: 78,
+        city: "Mogadishu"
+    },
+    {
+        id: 14,
+        name: "Nasra",
+        age: 20,
+        gender: "Female",
+        course: "Data Science",
+        year: 2,
+        grade: 83,
+        city: "Kismayo"
+    },
+    {
+        id: 15,
+        name: "Abdirahman",
+        age: 24,
+        gender: "Male",
+        course: "Information Technology",
+        year: 4,
+        grade: 72,
+        city: "Hargeisa"
+    },
+    {
+        id: 16,
+        name: "Hawa",
+        age: 19,
+        gender: "Female",
+        course: "Computer Science",
+        year: 1,
+        grade: 91,
+        city: "Mogadishu"
+    },
+    {
+        id: 17,
+        name: "Ali",
+        age: 21,
+        gender: "Male",
+        course: "Software Engineering",
+        year: 2,
+        grade: 89,
+        city: "Bosaso"
+    },
+    {
+        id: 18,
+        name: "Deeqa",
+        age: 22,
+        gender: "Female",
+        course: "Cybersecurity",
+        year: 3,
+        grade: 77,
+        city: "Mogadishu"
+    },
+    {
+        id: 19,
+        name: "Abdi",
+        age: 23,
+        gender: "Male",
+        course: "Data Science",
+        year: 3,
+        grade: 96,
+        city: "Garowe"
+    },
+    {
+        id: 20,
+        name: "Sumaya",
+        age: 20,
+        gender: "Female",
+        course: "Software Engineering",
+        year: 2,
+        grade: 84,
+        city: "Mogadishu"
+    }
+];
+students.forEach((student)=>{
+    console.log(students)
+})
