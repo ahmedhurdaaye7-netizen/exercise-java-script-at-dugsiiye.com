390bf745617b543fb643a2a92562df1fff940bc6
ahmedhurdaaye <ahmedhurdaaye7@gmail.com>
2026-09-14 06:05:04 -0700

    ex17

---
+++ java script/basic-exercise/ex17/index.js
@@ -0,0 +1,10 @@
+temp = 14;
+if(temp<0){
+    console.log("its very cold today!")
+} else if(temp>=0 && temp<=15){
+    console.log("it's cold today!")
+} else if(temp>=15 && temp<=25){
+    console.log("it's warm today!")
+} else{
+    console.log("its hot today!")
+}
